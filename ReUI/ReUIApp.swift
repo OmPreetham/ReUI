44593b0b2e921077a8f7911494d8243127c51ea4
Om Preetham <ompreethambandi@gmail.com>
2024-07-27 03:48:06 -0500

    Initial Commit

---
+++ ReUI/ReUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReUIApp.swift
+//  ReUI
+//
+//  Created by Om Preetham Bandi on 7/27/24.
+//
+
+import SwiftUI
+
+@main
+struct ReUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
